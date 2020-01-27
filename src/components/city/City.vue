0cff2d2d843f62936d078587717a42d7db80bb9e
lazyellow <958540498@qq.com>
2020-01-28 01:48:35 +0800

    add scroll

--- src/components/city/City.vue
+++ src/components/city/City.vue
@@ -1,22 +1,16 @@
 <template>
     <div class='city'>
         <city-header></city-header>
-        <city-hot :hotCities='hotCities'></city-hot>
-        <city-sort :cities='cities'></city-sort>
-        <city-list :cities='cities'></city-list>
+        <city-list :hotCities='hotCities' :cities='cities'></city-list>
     </div>
 </template>
 <script>
 import CityHeader from './pages/Header'
-import CityHot from './pages/Hot'
-import CitySort from './pages/Sort'
 import CityList from './pages/List'
 
 export default{
     components:{
         CityHeader,
-        CityHot,
-        CitySort,
         CityList
     },
     data(){
--- src/components/city/pages/List.vue
+++ src/components/city/pages/List.vue
@@ -1,57 +1,149 @@
 <template>
-    <div class="list">
-        <div v-for='(val,key) in cities'>
-            <div class="list-title">{{key}}</div>
-            <ul class="list-msg">
-                <li class="list-item"
-                    v-for='item in val' :key='item.id'>
-                    {{item.name}}
+  <div ref="container" class="container">
+      <div>
+        <!--hot-->
+        <div class="hot">
+            <div class="hot-title">热门城市</div>
+            <ul class="hot-list">
+                <li class="hot-item" v-for="item in hotCities" :key="item.id">{{item.name}}</li>
+            </ul>
+        </div>
+        <!--sort-->
+        <div class="sort">
+            <div class="sort-title">
+                字母排序
+            </div>
+            <ul class="sort-list">
+                <li class="sort-item"
+                    v-for='(val,key) in cities'>
+                    {{key}}
                 </li>
             </ul>
         </div>
+        <!--list-->
+        <div class="list">
+            <div v-for="(val,key) in cities">
+                <div class="list-title">{{key}}</div>
+                <ul class="list-msg">
+                <li class="list-item" v-for="item in val" :key="item.id">{{item.name}}</li>
+                </ul>
+            </div>
+        </div>
     </div>
+  </div>
 </template>
 <script>
+import BScroll from 'better-scroll'
 export default {
-    props:["cities"]
-}
+  props: ["hotCities","cities"],
+  data(){
+      return{
+          scroll:''
+      }
+  },
+  mounted(){
+        let container = this.$refs['container'];
+        this.scroll = new BScroll(container)
+  }
+};
 </script>
 <style lang="stylus">
 @import '~css/common.styl';
-    .list-title{
+.container{
+    position:absolute;
+    overflow:hidden;
+    left:0;
+    right:0;
+    bottom:0;
+    top:.88rem;
+    background:#f5f5f5;
+}
+.hot-title {
+  padding: 0.2rem 0.3rem;
+  font-size: 0.24rem;
+  color: #212121;
+}
+.hot-list {
+  position: relative;
+  overflow: hidden;
+  background: #fff;
+  font-size: 0.28rem;
+  color: #212121;
+}
+.hot-list:before {
+  content: " ";
+  position: absolute;
+  height: 100%;
+  width: 33.33333%;
+  left: 33.33333%;
+  border-left: 0.02rem solid #ddd;
+  border-right: 0.02rem solid #ddd;
+}
+.hot-item {
+  width: 33.33333%;
+  height: 0.9rem;
+  line-height: 0.9rem;
+  float: left;
+  border-bottom: 0.02rem solid #ddd;
+  text-align: center;
+}
+.sort-title{
         padding: .2rem .3rem; 
         font-size: .24rem;
         color: #212121;
     }
-    .list-msg{
+    .sort-list{
         position: relative;
         overflow: hidden;
-        background: #fff;
-    }
-    .list-msg:before{
-        position: absolute;
-        content:" ";
-        left: 25%;
-        width: 25%;
-        height: 100%;
-        border-left: .02rem solid #ddd;
-        border-right: .02rem solid #ddd;
-    }
-    .list-msg:after{
-        position: absolute;
-        content:" ";
-        left: 75%;
-        width: 25%;
-        height: 100%;
-        border-left: .02rem solid #ddd;
-    }
-    .list-item{
-        width: 25%;
-        line-height: .9rem;
+        background:#fff;
         font-size: .28rem;
-        text-align: center;
-        float: left;
-        border-bottom: .02rem solid #ddd;
-        textOverflow();
+        color: #212121;
     }
+    .sort-item{
+        width: 16.66666%;
+        height: .9rem;
+        line-height: .9rem;
+        float: left;
+        text-align: center;
+    }
+.list-title {
+  padding: 0.2rem 0.3rem;
+  font-size: 0.24rem;
+  color: #212121;
+}
+
+.list-msg {
+  position: relative;
+  overflow: hidden;
+  background: #fff;
+}
+
+.list-msg:before {
+  position: absolute;
+  content: ' ';
+  left: 25%;
+  width: 25%;
+  height: 100%;
+  border-left: 0.02rem solid #ddd;
+  border-right: 0.02rem solid #ddd;
+}
+
+.list-msg:after {
+  position: absolute;
+  content: ' ';
+  left: 75%;
+  width: 25%;
+  height: 100%;
+  border-left: 0.02rem solid #ddd;
+}
+
+.list-item {
+  width: 25%;
+  line-height: 0.9rem;
+  font-size: 0.28rem;
+  text-align: center;
+  float: left;
+  border-bottom: 0.02rem solid #ddd;
+  textOverflow();
+}
 </style>
