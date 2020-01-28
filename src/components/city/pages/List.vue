e76d945b5b290ed8a3d82ed783c449f61538b269
lazyellow <958540498@qq.com>
2020-01-28 18:15:33 +0800

    add sort功能

--- src/components/city/pages/List.vue
+++ src/components/city/pages/List.vue
@@ -15,14 +15,16 @@
             </div>
             <ul class="sort-list">
                 <li class="sort-item"
-                    v-for='(val,key) in cities'>
+                    v-for='(val,key) in cities'
+                    @click='changeSort(key)'>
                     {{key}}
                 </li>
             </ul>
         </div>
         <!--list-->
         <div class="list">
-            <div v-for="(val,key) in cities">
+            <div v-for="(val,key) in cities"
+                :ref='key'>
                 <div class="list-title">{{key}}</div>
                 <ul class="list-msg">
                 <li class="list-item" v-for="item in val" :key="item.id">{{item.name}}</li>
@@ -35,16 +37,23 @@
 <script>
 import BScroll from 'better-scroll'
 export default {
-  props: ["hotCities","cities"],
-  data(){
-      return{
-          scroll:''
-      }
-  },
-  mounted(){
+    props: ["hotCities","cities"],
+    data(){
+        return{
+            scroll:''
+        }
+    },
+    mounted(){
         let container = this.$refs['container'];
         this.scroll = new BScroll(container)
-  }
+    },
+    methods:{
+        changeSort(sortName){
+            // console.log(sortName);
+            // console.log(this.$refs[sortName][0]);
+            this.scroll.scrollToElement(this.$refs[sortName][0]);
+        }
+    }
 };
 </script>
 <style lang="stylus">
