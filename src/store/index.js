ebcb02d1c728f91f6ee0937c979522b8545f92ac
lazyellow <958540498@qq.com>
2020-01-28 22:56:36 +0800

    add localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,17 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+let defaultCity = '广州';   //2.定义一个中间变量，给一个默认值
+defaultCity = localStorage.city;    //3.将存储在localstorage中的值取出来
+
 const state = {
-    city:"上海"
+    city:defaultCity    //4.将其复制给state中，通过state再传回给首页header.vue中
 }
 const mutations = {
     changeCity(state,cityName){
         state.city = cityName;
+        //将数据存储在浏览器里本地存储localstorage中
+        localStorage.city = cityName;   //1.将点击后获取的城市名称值存储到localstorage中
     }
 }
 
